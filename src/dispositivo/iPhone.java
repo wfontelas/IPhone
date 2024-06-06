41a6760ef65b44f52c730cccebea84dafcf8afbf
William Fontelas <williamdevsuporte@gmail.com>
2024-06-06 05:58:29 -0300

    Finalizado o Projeto IPhone

---
+++ src/dispositivo/iPhone.java
@@ -0,0 +1,58 @@
+package dispositivo;
+
+import reprodutor.ReprodutorMusical;
+import telefone.AparelhoTelefonico;
+import navegador.NavegadorInternet;
+
+public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    private String numero;
+
+    // Métodos de ReprodutorMusical
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música.");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando música: " + musica);
+    }
+
+    // Métodos de AparelhoTelefonico
+    @Override
+    public void ligar(String numero) {
+        this.numero = numero;
+        System.out.println("Ligando para: " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada.");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz.");
+    }
+
+    // Métodos de NavegadorInternet
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página.");
+    }
+}
---
+++ src/navegador/NavegadorInternet.java
@@ -0,0 +1,7 @@
+package navegador;
+
+public interface NavegadorInternet {
+    void exibirPagina(String url);
+    void adicionarNovaAba();
+    void atualizarPagina();
+}
